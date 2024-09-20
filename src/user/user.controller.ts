1d9cce07be30e19d910586266c84874b3125241a
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-09-20 08:22:09 +0100

    :tada: feat:learning about dtos

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUserDto';
 
 @Controller('user')
 export class UserController {
-  @Get('all')
-  findAll() {
-    return 'all users';
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return {
+      user: {
+        id: id,
+      },
+    };
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return createUserDto;
   }
 }
