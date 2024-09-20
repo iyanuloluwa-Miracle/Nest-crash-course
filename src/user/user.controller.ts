48b60ff1c62d77da78d9ae41d0b27de10cd0033a
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-09-20 14:33:19 +0100

    :tada: feat:more learnig of dto and services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,18 @@
 import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return {
-      user: {
-        id: id,
-      },
-    };
+    return this.userService.findOne(id);
   }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return createUserDto;
+    return this.userService.create(createUserDto);
   }
 }
